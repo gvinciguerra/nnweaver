21d646f30a639e422c1bff60d6fab2be682b5ac3
Giorgio Vinciguerra <i@gvdev.com>
2018-01-15 20:05:41 +0100

    Added layers logic

--- nnweaver/layer.py
+++ nnweaver/layer.py
@@ -1,6 +1,22 @@
+from numpy.random import uniform
+
 from .activation import Linear
 
 
 class Layer(object):
     def __init__(self, units, activation=Linear()):
-        pass
+        assert units > 0
+        self.units = units
+        self.weights = None
+        self.activation = activation
+
+    def build_weights(self, preceding_units, low=-0.05, high=0.05):
+        if self.weights is not None:
+            raise Exception("This layer was already added to a NN")
+
+        self.weights = uniform(low, high, (preceding_units, self.units))
+        self.bias = uniform(low, high, self.units)
+
+    def __call__(self, x):
+        out = self.bias + x.dot(self.weights)
+        return self.activation(out)
--- nnweaver/nn.py
+++ nnweaver/nn.py
@@ -1,6 +1,18 @@
 class NN(object):
     def __init__(self, input_dim):
         self.input_dim = input_dim
+        self.layers = []
 
     def add_layer(self, layer):
-        raise NotImplementedError
+        if len(self.layers) == 0:
+            layer.build_weights(self.input_dim)
+        else:
+            preceding_units = self.layers[-1].units
+            layer.build_weights(preceding_units)
+
+        self.layers.append(layer)
+
+    def forward(self, x):
+        xi = x
+        for l in self.layers:
+            xi = l(xi)
