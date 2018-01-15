6637bcf2a0402f7f020200549de689ede4ad8b2f
Giorgio Vinciguerra <i@gvdev.com>
2018-01-15 20:05:52 +0100

    Added unit test

---
+++ test/layer_test.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pytest
+
+from nnweaver.activation import Linear
+from nnweaver.layer import Layer
+
+
+def test_call():
+    layer = Layer(3, Linear())
+    layer.weights = np.eye(3)
+    layer.bias = np.ones(3)
+    v = np.array([1, 2, 3])
+    np.testing.assert_array_equal(layer(v), np.array([2, 3, 4]))
