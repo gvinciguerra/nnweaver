Fixed bug in backpropagation and activation

nnweaver/activations.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class Activation(object):
     def __call__(self, x):
         raise NotImplementedError
@@ -16,7 +19,16 @@ class Linear(Activation):
 
 class Rectifier(Activation):
     def __call__(self, x):
-        return max(0, x)
+        return np.multiply(x, x > 0)
 
     def gradient(self, x):
-        return 1 if x > 0 else 0
+        return np.multiply(1, x > 0)
+
+
+class Sigmoid(Activation):
+    def __call__(self, x):
+        return 1.0 / (1.0 + np.exp(-x))
+
+    def gradient(self, x):
+        y = self(x)
+        return np.multiply(y, 1 - y)

test/optimizers_test.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from nnweaver.activations import Linear
+from nnweaver.activations import Linear, Sigmoid, Rectifier
 from nnweaver.layer import Layer
 from nnweaver.losses import MSE
 from nnweaver.nn import NN
@@ -12,6 +12,17 @@ def test_sgd_bisector():
     nn.add_layer(Layer(1, Linear()))
     X = np.matrix(np.arange(-1, 1, 0.1)).T
     Y = np.matrix(np.arange(-1, 1, 0.1)).T
-    sgd = SGD(MSE(), learning_rate=.5)
+    sgd = SGD(MSE(), learning_rate=0.5)
     sgd.fit(nn, X, Y, 1, 100)
     np.testing.assert_almost_equal(nn.predict(-1), -1)
+
+
+def test_quadratic():
+    nn = NN(1)
+    nn.add_layer(Layer(12, Sigmoid()))
+    nn.add_layer(Layer(12, Sigmoid()))
+    nn.add_layer(Layer(1, Linear()))
+    X = np.matrix(np.arange(-1, 1, 0.1)).T
+    Y = np.matrix(np.arange(-1, 1, 0.1) ** 2).T
+    sgd = SGD(MSE(), learning_rate=0.5)
+    sgd.fit(nn, X, Y, 5, 100)