4c5e3e8e5b3fb2d2a61871dc6d73376ecccfc098
flandolfi <fran.landolfi@gmail.com>
2018-01-19 18:53:04 +0100

    Added MEE loss function

--- nnweaver/losses.py
+++ nnweaver/losses.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 class Loss(object):
     @classmethod
     def __call__(cls, y, y_target):
@@ -19,3 +18,18 @@ class MSE(Loss):
     @classmethod
     def gradient(cls, y, y_target):
         return y - y_target
+
+
+class MEE(Loss):
+    @classmethod
+    def euclidean_distance(cls, y, y_target):
+        return np.sqrt(np.sum(np.square(y - y_target), axis=1))
+
+    @classmethod
+    def __call__(cls, y, y_target):
+        return MEE.euclidean_distance(y, y_target).mean()
+
+    @classmethod
+    def gradient(cls, y, y_target):
+        return (y - y_target) / MEE.euclidean_distance(y, y_target)
+
