45610282d9a9e2e499368bf099e2877a7a2dacf3
Francesco Landolfi <fran.landolfi@gmail.com>
2018-01-24 22:08:11 +0100

    Add PlotLearningCurve class and test

--- nnweaver/callbacks.py
+++ nnweaver/callbacks.py
@@ -4,6 +4,7 @@ or after the training of a neural network. """
 
 import csv
 from abc import ABC, abstractmethod
+import matplotlib.pyplot as plt
 
 
 class Callback(ABC):
@@ -74,8 +75,93 @@ class WriteFileCallback(Callback):
         self.csv_writer.writerow(record)
 
     def on_training_end(self, nn):
-        """ The callback to be executed at the endo of the training.
+        """ The callback to be executed at the end of the training.
 
         :param nn: the neural network that is being trained.
         """
         self.file.close()
+
+
+class PlotLearningCurve(Callback):
+    """ :py:class:`PlotLearningCurve` provides a way to plot the learning curve
+    of a model during and after its training phase. """
+
+    def __init__(self, x_validation=None, y_validation=None, loss=None, interactive=True, max_epochs=None):
+        """ Initialize the callback. If a data set and a loss function are
+        given, it will perform a validation of the model at every epoch.
+
+        :param x_validation: the set of validation examples (optional).
+        :param y_validation: the set of validation targets (optional).
+        :param loss: the loss function to be evaluated in the validation step
+            (optional).
+        :param interactive: if ``True``, show the learning curve interactively
+            during the training of the model, otherwise only after the training.
+            Notice that the former may cause a loss of training performance.
+        :param max_epochs: the expected number of epochs. If present, this
+            parameter prevents the plot from rescaling during the training (if
+            ``interactive`` is ``False``, this parameter is ignored).
+        """
+        assert bool(x_validation is None) == bool(y_validation is None) == bool(loss is None)
+        self.x_validation = x_validation
+        self.y_validation = y_validation
+        self.loss = loss
+        self.epochs = []
+        self.losses = []
+        self.val_losses = []
+        self.fig, self.ax = plt.subplots()
+        self.interactive = interactive
+        self.max_epochs = max_epochs
+
+    def on_training_begin(self, nn):
+        """ The callback to be executed at the beginning of the training.
+
+        :param nn: the neural network that is being trained.
+        """
+        if self.interactive:
+            plt.ion()
+            plt.show()
+
+    def on_epoch_end(self, epoch, nn, loss_value, metrics_values=None):
+        """ The callback to be executed at the end of every epoch.
+
+        :param epoch: the epoch number.
+        :param nn: the neural network that is being trained.
+        :param loss_value: the loss value obtained in this epoch.
+        :param metrics_values: ignored.
+        """
+        self.epochs.append(epoch)
+        self.losses.append(loss_value)
+        if self.x_validation is not None:
+            y_predicted = nn.predict_batch(self.x_validation)
+            validation_loss_value = self.loss.batch_mean(y_predicted, self.y_validation)
+            self.val_losses.append(validation_loss_value)
+
+        if self.interactive:
+            self.ax.cla()
+            self.ax.plot(self.epochs, self.losses, 'b', label='Training')
+            if self.x_validation is not None:
+                self.ax.plot(self.epochs, self.val_losses, 'r', label='Validation')
+                self.ax.legend()
+            if self.max_epochs:
+                self.ax.set_xlim(0, self.max_epochs)
+            self.ax.set_ylim(0)
+            self.ax.set_title('Learning Curve')
+            self.ax.set_xlabel('Epoch')
+            self.ax.set_ylabel('Loss')
+            self.fig.canvas.draw()
+
+    def on_training_end(self, nn):
+        """ The callback to be executed at the end of the training.
+
+        :param nn: the neural network that is being trained.
+        """
+        if self.interactive:
+            plt.ioff()
+        else:
+            self.ax.set_title('Learning Curve')
+            self.ax.set_xlabel('Epoch')
+            self.ax.set_ylabel('Loss')
+            self.ax.plot(self.epochs, self.losses, 'b')
+            if self.x_validation is not None:
+                self.ax.plot(self.epochs, self.val_losses, 'r')
+        plt.show(block=True)
--- test/callbacks_test.py
+++ test/callbacks_test.py
@@ -21,3 +21,22 @@ def test_write_file_callback():
         sgd.train(nn, x[:limit], y[:limit], 0.1, 5, epochs, callbacks=[callback])
         tmp.file.seek(0)
         assert sum(1 for _ in tmp.file) == epochs + 1
+
+
+def test_plot_learning_curve():
+    samples = 50
+    a = np.random.uniform(0, 2 * np.pi, samples * 2)
+    r = np.append(np.random.uniform(0, 10, samples), np.random.uniform(20, 30, samples))
+    x = np.matrix([np.multiply(r, np.sin(a)), np.multiply(r, np.cos(a))]).T
+    y = np.append(np.ones(samples), np.zeros(samples))
+    x, y = Optimizer.shuffle(x, y)
+    limit = int(len(x) * 0.8)
+
+    nn = NN(2)
+    nn.add_layer(Layer(4, Rectifier()))
+    nn.add_layer(Layer(1, Sigmoid()))
+    sgd = SGD(MSE)
+    sgd.seed = 42
+    epochs = 100
+    callback = callbacks.PlotLearningCurve(x[limit:], y[limit:], loss=MSE, max_epochs=epochs)
+    sgd.train(nn, x[:limit], y[:limit], 0.1, 5, epochs, callbacks=[callback])
