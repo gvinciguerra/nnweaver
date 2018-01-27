00ff7a33e074d2052404f964767452a26d44ca4f
Giorgio Vinciguerra <i@gvdev.com>
2018-01-27 12:34:37 +0100

    WriteFileCallback now supports both metrics on training and test sets

--- nnweaver/callbacks.py
+++ nnweaver/callbacks.py
@@ -27,7 +27,7 @@ class Callback(ABC):
 
 
 class WriteFileCallback(Callback):
-    def __init__(self, filename, x_validation=None, y_validation=None, loss=None):
+    def __init__(self, filename, x_validation=None, y_validation=None, loss=None, metrics=None):
         """ Create a callback that, at each epoch, writes the loss found by a
         training algorithm to a CSV file. If a data set and a loss function are
         given, it will also write to the file the validation results.
@@ -36,13 +36,16 @@ class WriteFileCallback(Callback):
         :param x_validation: the set of validation examples.
         :param y_validation: the set of validation targets.
         :param loss: the loss function to be evaluated in the validation step.
+        :param metrics: a list of metrics to be evaluated in the validation step.
         """
         assert bool(x_validation is None) == bool(y_validation is None) == bool(loss is None)
+        assert bool(metrics is None) == bool(x_validation is None) == bool(y_validation is None)
         self.x_validation = x_validation
         self.y_validation = y_validation
         self.file = None
         self.loss = loss
         self.csv_writer = None
+        self.metrics = [] if metrics is None else metrics
         self.filename = filename
 
     def on_training_begin(self, nn):
@@ -51,11 +54,7 @@ class WriteFileCallback(Callback):
         :param nn: the neural network that is being trained.
         """
         self.file = open(self.filename, 'w')
-        fieldnames = ['epoch', 'loss']
-        if self.x_validation is not None:
-            fieldnames.append('val_loss')
-        self.csv_writer = csv.DictWriter(self.file, fieldnames)
-        self.csv_writer.writeheader()
+        self.csv_writer = None
 
     def on_epoch_end(self, epoch, nn, loss_value, metrics_values):
         """ The callback to be executed at the end of every epoch.
@@ -65,11 +64,24 @@ class WriteFileCallback(Callback):
         :param loss_value: the loss value obtained in this epoch.
         :param metrics_values: the other metric values obtained in this epoch.
         """
+        if self.csv_writer is None:
+            fieldnames = ['epoch', 'loss']
+            if self.x_validation is not None:
+                fieldnames.append('val_loss')
+            fieldnames.extend(['train_' + m for m in metrics_values.keys()])
+            fieldnames.extend(['val_' + m.__name__ for m in self.metrics])
+            self.csv_writer = csv.DictWriter(self.file, fieldnames)
+            self.csv_writer.writeheader()
+
         record = {'epoch': epoch, 'loss': loss_value}
+        for k, v in metrics_values.items():
+            record['train_' + k] = v
         if self.x_validation is not None:
             y_predicted = nn.predict_batch(self.x_validation)
             validation_loss_value = self.loss.batch_mean(y_predicted, self.y_validation)
             record['val_loss'] = validation_loss_value
+            for m in self.metrics:
+                record['val_' + m.__name__] = m(y_predicted, self.y_validation)
         self.csv_writer.writerow(record)
 
     def on_training_end(self, nn):
