cab6046b341ed4a5b549a896c992c1d1c0e0b71f
Giorgio Vinciguerra <i@gvdev.com>
2018-07-10 17:25:25 +0200

    Updated parameters
    Co-Authored-By: Francesco Landolfi <flandolfi@users.noreply.github.com>

--- examples/mlcup_bundle.py
+++ examples/mlcup_bundle.py
@@ -7,17 +7,24 @@ import numpy as np
 from nnweaver import *
 from nnweaver.optimizers import ProximalBundleMethod
 
+regularizer = L1L2Regularizer(0.00037205665611222174, 0.0008170173137216012)
 
-train_args = {'mu': 10,
-              'm_L': 0.1,
-              'm_R': 0.9,
-              't_bar': 0.5,
-              'gamma': 0.5,
-              'accuracy_tolerance': 1e-4,
-              'max_iterations': 100}
+train_args = {
+    "mu": 1.226627590424536,
+    "m_L": 0.21547347050343577,
+    "m_R": 0.549492896940917,
+    "t_bar": 0.28027206476236083,
+    "a_bar": 4.829740742364858,
+    "regularizer": regularizer,
+    "gamma": 0.0053088012268946985,
+    "accuracy_tolerance": 1e-08,
+    "max_iterations": 200
+}
+
 
 assert len(sys.argv) > 1, 'Dataset path not given'
-dataset = np.genfromtxt(sys.argv[1] + '/train.csv', delimiter=',', usecols=tuple(range(1, 13)))
+dataset = np.genfromtxt(sys.argv[1] + '/train.csv', delimiter=',',
+                        usecols=tuple(range(1, 13)))
 x = dataset[:, :10]
 y = dataset[:, 10:]
 limit = int(len(x) * 0.8)
@@ -35,7 +42,7 @@ directory = sys.argv[1] + '/results/'
 os.makedirs(directory, exist_ok=True)
 file_path = '%s/%s.csv' % (directory, len(os.listdir(directory)))
 wtf = callbacks.WriteFileCallback(file_path, x_val, y_val, loss=MEE)
-plc = callbacks.PlotLearningCurve(x_val, y_val, loss=MEE, interactive=False)
+plc = callbacks.PlotLearningCurve(x_val, y_val, loss=MEE, interactive=True)
 
 pbm = ProximalBundleMethod(MEE)
 pbm.train(nn, x_train, y_train, callbacks=[plc, wtf], **train_args)
@@ -46,9 +53,15 @@ with open(file_path, 'a') as f:
 
 fig, ax = plt.subplots()
 y_predict = nn.predict_batch(x_val)
-ax.scatter(y_predict[:, 0], y_predict[:, 1], c='b', marker='+', s=30, linewidth=.4)
+ax.scatter(y_predict[:, 0], y_predict[:, 1],
+           c='b', marker='+', s=30, linewidth=.4)
 ax.scatter(y_val[:, 0], y_val[:, 1], c='r', marker='+', s=30, linewidth=.4)
 plt.show()
+
+with open('%s/bundle_predictions.csv' % (directory), 'w') as f:
+    f.write('y0_predict,y1_predict,y0_val,y1_val\n')
+    for y, y_val in zip(y_predict, y_val):
+        f.write('%f,%f,%f,%f\n' % (y[0], y[1], y_val[0], y_val[1]))
 
 # DO NOT TOUCH UNTIL THE END
 # dataset_test = np.genfromtxt(sys.argv[1] + '/test.csv', delimiter=',', usecols=tuple(range(1, 13)))
